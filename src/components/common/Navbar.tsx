6eb29165bf283c83d67ec3d7b69292cba602aa21
tonylxm <126369686+tonylxm@users.noreply.github.com>
2024-11-20 09:39:28 +1300

    feat: moved cross on side nav to the right and repositioned

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -58,7 +58,7 @@ export default function Navbar() {
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth="2"
-                d={isSideNavOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}
+                d="M4 6h16M4 12h16M4 18h16"
               />
             </svg>
           </button>
@@ -69,26 +69,28 @@ export default function Navbar() {
         className={`fixed top-0 right-0 h-full w-64 bg-sesa-navy text-white transform ${isSideNavOpen ? "-translate-x-0" : "translate-x-full"
           } transition-transform duration-300 ease-in-out z-40`}
       >
-        <div className="p-4">
-          <button
-            className="btn btn-square btn-ghost"
-            onClick={() => setIsSideNavOpen(false)}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+        <div className="py-2 px-2">
+          <div className="w-full  flex justify-end">
+            <button
+              className="btn btn-square btn-ghost"
+              onClick={() => setIsSideNavOpen(false)}
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          </button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
         <ul className="menu p-4 text-base">
           {links.map(({ href, label }) => (

a81aaf2d28b23227ee37dc4255fbb48d2fc640ec
tonylxm <126369686+tonylxm@users.noreply.github.com>
2024-11-20 11:25:26 +1300

    feat: added moving gradient background and cursor animation following

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from 'next/font/local';
 import '@/styles/globals.css';
 import Navbar from '@/components/common/Navbar';
 import Footer from '@/components/common/Footer';
+import { CursorAnimation } from '@/utils/CursorAnimation';
 
 const geistSans = localFont({
   src: '../fonts/GeistVF.woff',
@@ -28,11 +29,35 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-wdcc-blue-to-sesa-teal`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
-        {children}
-        <Footer />
+        <div className="gradient-bg fixed inset-0 -z-10">
+          <svg xmlns="http://www.w3.org/2000/svg">
+            <defs>
+              <filter id="goo">
+                <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
+                <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8" result="goo" />
+                <feBlend in="SourceGraphic" in2="goo" />
+              </filter>
+            </defs>
+          </svg>
+          <div className="gradients-container">
+            <div className="g1"></div>
+            <div className="g2"></div>
+            <div className="g3"></div>
+            <div className="g4"></div>
+            <div className="g5"></div>
+            <div className="interactive"></div>
+          </div>
+        </div>
+
+        <div className="relative z-10">
+          <Navbar />
+          {children}
+          <Footer />
+        </div>
+
+        <CursorAnimation />
       </body>
     </html>
   );

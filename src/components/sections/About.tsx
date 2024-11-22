69a559a162caa3ac432fe57d9bd613e5c3eb098c
Jos Badenas <jbad180@aucklanduni.ac.nz>
2024-11-22 17:33:25 +1300

    feat: Added stats and description

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import { AboutUs, EventStats } from '@/data/about';
 
 const About = () => {
   return (
     <section id="about" className="h-screen w-full p-20">
-      <h1 className="text-4xl font-bold">About</h1>
+      <div id="about-left">
+        <h1 className="text-4xl font-bold">About</h1>
+        <p>{AboutUs}</p>
+      </div>
+      <div id="about-right">
+        <div>
+          <h1 className="text-4xl font-bold">{EventStats.hackers}</h1>
+          <p className="text-sm text-gray-600">hackers</p>
+        </div>
+        <div>
+          <h1 className="text-4xl font-bold">{EventStats.teams}</h1>
+          <p className="text-sm text-gray-600">teams</p>
+        </div>
+        <div>
+          <h1 className="text-4xl font-bold">{EventStats.sponsors}</h1>
+          <p className="text-sm text-gray-600">sponsors</p>
+        </div>
+        <div>
+          <h1 className="text-4xl font-bold">{EventStats.prizes}</h1>
+          <p className="text-sm text-gray-600">prizes</p>
+        </div>
+        <div>
+          <h1 className="text-4xl font-bold">{EventStats.duration}</h1>
+          <p className="text-sm text-gray-600">hours</p>
+        </div>
+      </div>
     </section>
   );
 };

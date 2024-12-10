431d4a03d2f280d0d40f6ca5a205123de6c5280c
JooHyun Kang <161455101+jkan172@users.noreply.github.com>
2024-12-10 13:10:13 +1300

    feat: Removed commented code

--- src/components/common/CountdownTimer.tsx
+++ src/components/common/CountdownTimer.tsx
@@ -16,10 +16,6 @@ interface TimeLeft {
 
 export default function CountdownTimer() {
   const [hackathonTime, setHackathonTime] = useState(false);
-  // const [days, setDays] = useState(0);
-  // const [hours, setHours] = useState(0);
-  // const [minutes, setMinutes] = useState(0);
-  // const [seconds, setSeconds] = useState(0);
 
 
   const [timeLeft, setTimeLeft] = useState<TimeLeft>({
@@ -39,7 +35,6 @@ export default function CountdownTimer() {
   
   const googleUrl = useMemo(() => google(event), [event]); 
 
-  // console.log(googleUrl);
 
 
   useEffect(() => {
@@ -48,28 +43,14 @@ export default function CountdownTimer() {
       const difference = target.getTime() - now.getTime();
 
       const d = Math.floor(difference / (1000 * 60 * 60 * 24));
-      // setDays(d);
 
       const h = Math.floor(
         (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
       );
-      // setHours(h);
 
       const m = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-      // setMinutes(m);
 
       const s = Math.floor((difference % (1000 * 60)) / 1000);
-      // setSeconds(s);
-
-      // if (d <= 0 && h <= 0 && m <= 0 && s <= 0) {
-      //   setHackathonTime(true);
-      //   clearInterval(interval);
-      // }
-
-      // if (difference <= 0) {
-      //   setHackathonTime(true);
-      //   clearInterval(interval);
-      // }
 
       setTimeLeft({ days: d, hours: h, minutes: m, seconds: s });
 
@@ -78,48 +59,6 @@ export default function CountdownTimer() {
     return () => clearInterval(interval);
   }, []);
 
-  // return (
-  //   <div className="flex flex-col items-center justify-center min-h-screen text-white">
-  //     <h1 className="flex flex-col text-2xl">AUG 2-3, 2025</h1>
-      
-  //     {hackathonTime ? (
-  //       <h1 className="text-6xl font-bold text-center">Hackathon Time!</h1>
-  //     ) : (
-  //       <div className="flex flex-wrap items-center justify-center gap-4 md:flex-nowrap">
-  //         <div className="flex flex-col items-center">
-  //           <span className="text-5xl font-bold md:text-7xl leading-none">{days}</span>
-  //           <span className="text-base md:text-xl leading-none">Days</span>
-  //         </div>
-  //         <span className="text-5xl md:text-7xl font-bold leading-none">:</span>
-  //         <div className="flex flex-col items-center">
-  //           <span className="text-5xl font-bold md:text-7xl leading-none">{hours}</span>
-  //           <span className="text-base md:text-xl leading-none">Hours</span>
-  //         </div>
-  //         <span className="text-5xl md:text-7xl font-bold leading-none">:</span>
-  //         <div className="flex flex-col items-center">
-  //           <span className="text-5xl font-bold md:text-7xl leading-none">{minutes}</span>
-  //           <span className="text-base md:text-xl leading-none">Minutes</span>
-  //         </div>
-  //         <span className="text-5xl md:text-7xl font-bold leading-none">:</span>
-  //         <div className="flex flex-col items-center">
-  //           <span className="text-5xl font-bold md:text-7xl leading-none">{seconds}</span>
-  //           <span className="text-base md:text-xl leading-none">Seconds</span>
-  //         </div>
-  //       </div>
-  //     )}
-
-
-  //     <button 
-  //     className="bg-white border rounded px-4 py-3 font-bold text-black mt-10 ;"
-  //     onClick={() => window.open(googleUrl, "_blank")}>
-  //       Add to Google Calendar
-  //     </button>
-
-  //   </div>
-
-
-
-  // );
 
   return (
     <div className="flex flex-wrap items-center justify-center gap-4 md:flex-nowrap">
@@ -134,6 +73,7 @@ export default function CountdownTimer() {
           )}
         </React.Fragment>
       ))}
+      
       
   <button 
     className="bg-white border rounded px-4 py-3 font-bold text-black mt-10 ;"

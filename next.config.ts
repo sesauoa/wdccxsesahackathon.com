502e7937ffd7a94ff8a04a7bb61e18dc461ddc8e
tonylxm <126369686+tonylxm@users.noreply.github.com>
2025-01-14 16:35:22 +1300

    chore: Added deployment scripts and GitHub pages dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+
+  output: 'export',
+  basePath: '/wdccxsesahackathon.com',
+  assetPrefix: '/wdccxsesahackathon.com/',
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;

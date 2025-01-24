ab1d3f00f7c811a649ad4431611ab9681ad234b4
tonylxm <126369686+tonylxm@users.noreply.github.com>
2025-01-24 23:30:05 +1300

    refactor: Added registrationFaqs

---
+++ src/data/registrationFaqs.ts
@@ -0,0 +1,19 @@
+import { q } from 'motion/react-client';
+
+export const registrationFaqsInfo = [
+  {
+    question: 'Who can sign up?',
+    answer:
+      'We are open to all enrolled tertiary students who are members of WDCC and SESA (membership is free for both clubs!). Due to venue space and capacity restrictions, registration does not guarantee acceptance. You will be notified via email after the deadline.',
+  },
+  {
+    question: 'How many people can be in a team?',
+    answer:
+      "Six. You can sign up individually or as part of a team with a maximum of six members. If you're signing up alone, we’ll allocate you to a team. If you sign up in a team with fewer than six members, we’ll add additional individuals to your team.",
+  },
+  {
+    question: 'Does signing up guarantee my spot?',
+    answer:
+      'No, admitted teams will be notified via email after the deadline to confirm their place in the hackathon.',
+  },
+];

ea1c7f4fc9ef02e9bc083e1c7f7b1a991968783c
tonylxm <126369686+tonylxm@users.noreply.github.com>
2025-01-24 23:20:51 +1300

    refactor: Separated past winners data

--- src/app/registration/page.tsx
+++ src/app/registration/page.tsx
@@ -14,7 +14,7 @@ export default function Registration() {
 
   return (
     <div className="flex min-h-screen w-full flex-col items-center p-20">
-        <H1 className="mb-8 ">Registration</H1>
+      <H1 className="mb-8 ">Registration</H1>
       <div className="flex w-full justify-center">
         <div className="w-full max-w-6xl px-4 sm:px-6">
           <iframe
@@ -26,15 +26,15 @@ export default function Registration() {
           >
             Loading…
           </iframe>
+
           {/* FAQ Section */}
           <div className="mt-8" id="faqs">
             <div className="flex flex-col space-y-4">
               {faqInfo.slice(-3).map((faq, index) => (
                 <div
                   key={index}
-                  className={`collapse collapse-arrow ${
-                    openAccordion === index ? 'collapse-open' : 'collapse-close'
-                  } rounded-lg border border-gray-300 bg-white`}
+                  className={`collapse collapse-arrow ${openAccordion === index ? 'collapse-open' : 'collapse-close'
+                    } rounded-lg border border-gray-300 bg-white`}
                 >
                   <div
                     className="collapse-title cursor-pointer text-xl font-medium text-black"

67d102795e38d942412389ac7083d7da6362e142
dsaw04 <161405969+dsaw04@users.noreply.github.com>
2025-01-26 19:26:15 +1300

    fix: Fixed the filter buttons issue.

--- src/components/winner/FilterButtons.tsx
+++ src/components/winner/FilterButtons.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 interface FilterButtonsProps {
   years: number[];
@@ -17,10 +17,22 @@ export const FilterButtons: React.FC<FilterButtonsProps> = ({
   setSelectedYear,
   setSelectedPlace,
 }) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const handleWheel = (event: React.WheelEvent<HTMLDivElement>) => {
+    if (event.shiftKey) {
+      event.preventDefault(); // Prevent the default scroll behavior
+      event.stopPropagation(); // Stop Lenis from handling the event
+      if (containerRef.current) {
+        containerRef.current.scrollLeft += event.deltaY; // Scroll horizontally
+      }
+    }
+  };
+
   const toggleYear = (year: number) => {
     setSelectedYear(
       selectedYear.includes(year)
-        ? selectedYear.filter((y) => y !== year) 
+        ? selectedYear.filter((y) => y !== year)
         : [...selectedYear, year]
     );
   };
@@ -28,17 +40,21 @@ export const FilterButtons: React.FC<FilterButtonsProps> = ({
   const togglePlace = (place: string) => {
     setSelectedPlace(
       selectedPlace.includes(place)
-        ? selectedPlace.filter((p) => p !== place) 
-        : [...selectedPlace, place] 
+        ? selectedPlace.filter((p) => p !== place)
+        : [...selectedPlace, place]
     );
   };
 
   return (
-    <div className="scrollbar-hide flex w-full space-x-4 overflow-x-scroll lg:grid lg:grid-cols-8 lg:gap-4 lg:overflow-visible">
+    <div
+      ref={containerRef}
+      onWheel={handleWheel}
+      className="no-scrollbar flex w-full space-x-4 overflow-x-scroll lg:grid lg:grid-cols-8 lg:gap-4 lg:overflow-visible"
+    >
       {years.map((year) => (
         <button
           key={year}
-          className={`whitespace-nowrap rounded-lg px-4 py-2 font-bold drop-shadow-lg transition-all duration-200 ease-in-out ${
+          className={`min-w-fit whitespace-nowrap rounded-lg px-4 py-2 font-bold drop-shadow-lg transition-all duration-200 ease-in-out ${
             selectedYear.includes(year)
               ? 'bg-white text-sesa-navy'
               : 'bg-sesa-navy text-white hover:bg-slate-700'
@@ -52,7 +68,7 @@ export const FilterButtons: React.FC<FilterButtonsProps> = ({
       {places.map((place) => (
         <button
           key={place}
-          className={`whitespace-nowrap rounded-lg px-4 py-2 font-bold drop-shadow-lg transition-all duration-200 ease-in-out ${
+          className={`min-w-fit whitespace-nowrap rounded-lg px-4 py-2 font-bold drop-shadow-lg transition-all duration-200 ease-in-out ${
             selectedPlace.includes(place)
               ? 'bg-white text-sesa-navy'
               : 'bg-sesa-navy text-white hover:bg-slate-700'

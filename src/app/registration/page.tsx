236ea73c3af0bb85e4d23ac959cbc38bacf03327
AntGa <antonvince.garay@gmail.com>
2025-01-26 17:47:25 +1300

    .

--- src/app/registration/page.tsx
+++ src/app/registration/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useState } from 'react';
-import { FORM_URL } from '../../data/registrationdata';
+import { FORM_URL } from './_data/registrationdata';
 import { registrationFaqsInfo } from '@/data/registrationFaqs';
 import { H1, H2 } from '@/components/common/Typography';
 import CountdownTimer from '@/components/common/CountdownTimer';
@@ -15,12 +15,11 @@ export default function Registration() {
 
   return (
     <div className="flex min-h-screen w-full flex-col items-center">
-      <H1 className="mb-8 ">Registration</H1>
+      <H1 className="mb-8">Registration</H1>
       <div className="flex w-full justify-center">
         <div className="w-full max-w-6xl px-4 sm:px-6">
-
           {/* FOR WHEN REGISTRATIONS ARE CLOSED */}
-          <H2 className='text-4xl text-center m-10'>COMING SOON...</H2>
+          <H2 className="m-10 text-center text-4xl">COMING SOON...</H2>
           <CountdownTimer />
 
           {/* FOR WHEN REGISTRATIONS ARE OPEN */}
@@ -40,17 +39,20 @@ export default function Registration() {
               {registrationFaqsInfo.map((faq, index) => (
                 <div
                   key={index}
-                  className={`collapse collapse-arrow ${openAccordion === index ? 'collapse-open' : 'collapse-close'
-                    } rounded-lg border border-gray-300 bg-white`}
+                  className={`collapse collapse-arrow ${
+                    openAccordion === index ? 'collapse-open' : 'collapse-close'
+                  } rounded-lg border border-gray-300 bg-white`}
                 >
                   <div
-                    className="collapse-title cursor-pointer text-xl lg:text-2xl font-medium text-black"
+                    className="collapse-title cursor-pointer text-xl font-medium text-black lg:text-2xl"
                     onClick={() => handleToggle(index)}
                   >
                     {faq.question}
                   </div>
                   <div className="collapse-content">
-                    <p className="text-black text-lg lg:text-xl">{faq.answer}</p>
+                    <p className="text-lg text-black lg:text-xl">
+                      {faq.answer}
+                    </p>
                   </div>
                 </div>
               ))}
